410e8f47f5fb08c2952ac2bf4e45c9d0a8dc9f2b
Jemaroo <105898404+Jemaroo@users.noreply.github.com>
2025-07-26 14:34:37 -0700

    Add files via upload

---
+++ FileItemData.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * @Author Jemaroo
+ * @Function Storage object for handling ItemData, ShopData, and DropData data
+ */
+public class FileItemData 
+{
+    ArrayList<ItemData> items = new ArrayList<ItemData>();
+    ArrayList<ShopData> shops = new ArrayList<ShopData>();
+    ArrayList<DropData> drops = new ArrayList<DropData>();
+}
