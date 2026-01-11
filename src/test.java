9f918aa040f25f914990a9ded97a2eb3f831ae53
Jemaroo <105898404+Jemaroo@users.noreply.github.com>
2026-01-10 21:44:08 -0800

    Create test.java

---
+++ src/test.java
@@ -0,0 +1,162 @@
+import java.util.ArrayList;
+
+public class test 
+{
+    public static void testUnitData(FileItemData fileData)
+    {
+        testItemData(fileData.items);
+        testShopData(fileData.shops);
+        testDropData(fileData.drops);
+    }
+
+    private static void testItemData(ArrayList<ItemData> items)
+    {
+        if(items.size() > 0)
+        {
+            String retVal = "";
+            for(int i = 0; i < items.size(); i++)
+            {
+                retVal += "Name: " + items.get(i).name + "\n"
+                + "UseLocationShop: " + items.get(i).UseLocationShop + " | "
+                + "UseLocationBattle: " + items.get(i).UseLocationBattle + " | "
+                + "UseLocationField: " + items.get(i).UseLocationField + " | "
+                + "sortOrder: " + items.get(i).sortOrder + " | "
+                + "buyPrice: " + items.get(i).buyPrice + " | "
+                + "discountPrice: " + items.get(i).discountPrice + " | "
+                + "starPiecePrice: " + items.get(i).starPiecePrice + " | "
+                + "sellPrice: " + items.get(i).sellPrice + " | "
+                + "BPCost: " + items.get(i).BPCost + " | "
+                + "HPRestored: " + items.get(i).HPRestored + " | "
+                + "FPRestored: " + items.get(i).FPRestored + " | "
+                + "SPRestored: " + items.get(i).SPRestored + "\n\n";
+            }
+            System.out.println(retVal);
+        }
+    }
+
+    private static void testShopData(ArrayList<ShopData> shops)
+    {
+        String retVal = "";
+        for(int i = 0; i < shops.size(); i++)
+        {
+            retVal += "Name: " + shops.get(i).name + "\n"
+            + "Size: " + shops.get(i).size + "\nItem ID's: ";
+
+            for(int j = 0; j < shops.get(i).ids.size(); j++)
+            {
+                if((j + 1) == shops.get(i).ids.size())
+                {
+                    retVal += shops.get(i).ids.get(j) + "\n";
+                }
+                else
+                {
+                    retVal += shops.get(i).ids.get(j) + ", ";
+                }
+            }
+
+            if(shops.get(i).type != null)
+            {
+                retVal += "Type: " + shops.get(i).type + "\n";
+            }
+
+            if(shops.get(i).throwWeights.size() > 0)
+            {
+                retVal += "Throw Weights: ";
+                for(int j = 0; j < shops.get(i).throwWeights.size(); j++)
+                {
+                    if((j + 1) == shops.get(i).throwWeights.size())
+                    {
+                        retVal += shops.get(i).throwWeights.get(j) + "\n";
+                    }
+                    else
+                    {
+                        retVal += shops.get(i).throwWeights.get(j) + ", ";
+                    }
+                }
+            }
+
+            if(shops.get(i).sellPrices.size() > 0)
+            {
+                retVal += "Sell Prices: ";
+                for(int j = 0; j < shops.get(i).sellPrices.size(); j++)
+                {
+                    if((j + 1) == shops.get(i).sellPrices.size())
+                    {
+                        retVal += shops.get(i).sellPrices.get(j) + "\n";
+                    }
+                    else
+                    {
+                        retVal += shops.get(i).sellPrices.get(j) + ", ";
+                    }
+                }
+            }
+
+           retVal += "\n";
+        }
+        System.out.println(retVal);
+    }
+
+    private static void testDropData(ArrayList<DropData> drops)
+    {
+        if(drops.size() > 0)
+        {
+            String retVal = "";
+            for(int i = 0; i < drops.size(); i++)
+            {
+                retVal += "Name: " + drops.get(i).name + "\n"
+                + "Size: " + drops.get(i).size + "\nItem ID's: ";
+
+                if(drops.get(i).ids.size() > 0)
+                {
+                    for(int j = 0; j < drops.get(i).ids.size(); j++)
+                    {
+                        if((j + 1) == drops.get(i).ids.size())
+                        {
+                            retVal += drops.get(i).ids.get(j) + "  |  ";
+                        }
+                        else
+                        {
+                            retVal += drops.get(i).ids.get(j) + ", ";
+                        }
+                    }
+                }
+
+                if(drops.get(i).holdWeights.size() > 0)
+                {
+                    retVal += "Hold Weights: ";
+                    for(int j = 0; j < drops.get(i).holdWeights.size(); j++)
+                    {
+                        if((j + 1) == drops.get(i).holdWeights.size())
+                        {
+                            retVal += drops.get(i).holdWeights.get(j) + "  |  ";
+                        }
+                        else
+                        {
+                            retVal += drops.get(i).holdWeights.get(j) + ", ";
+                        }
+                    }
+                }
+
+                if(drops.get(i).dropWeights.size() > 0)
+                {
+                    retVal += "Hold Weights: ";
+                    for(int j = 0; j < drops.get(i).dropWeights.size(); j++)
+                    {
+                        if((j + 1) == drops.get(i).dropWeights.size())
+                        {
+                            retVal += drops.get(i).dropWeights.get(j) + "  |  ";
+                        }
+                        else
+                        {
+                            retVal += drops.get(i).dropWeights.get(j) + ", ";
+                        }
+                    }
+                }
+
+                retVal += "\n\n";
+            }
+
+            System.out.println(retVal);
+        }
+    }
+}
